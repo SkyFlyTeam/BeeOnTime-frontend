829a65c131ba6e14e24900f32e4b0831731ee53b
karengoncalves8 <karengoncalves128@gmail.com>
2025-03-17 23:45:05 -0300

    feat: adicionado o componente de datatable

--- src/pages/carro/index.tsx
+++ src/pages/carro/index.tsx
@@ -1,14 +1,42 @@
+import { DataTable } from '@/src/components/ui/datatable';
 import { useRouter } from 'next/router';
+
+import { columns, Payment } from './columns'
 
 export default function Carro() {
     const router = useRouter();
 
+    const data: Payment[] = [
+        {
+          id: "m5gr84i9",
+          amount: 316,
+          status: "success",
+          email: "ken99@example.com",
+        },
+        {
+          id: "3u1reuv4",
+          amount: 242,
+          status: "success",
+          email: "Abe45@example.com",
+        },
+        {
+          id: "derv1ws0",
+          amount: 837,
+          status: "processing",
+          email: "Monserrat44@example.com",
+        },
+        {
+          id: "5kma53ae",
+          amount: 874,
+          status: "success",
+          email: "Silas22@example.com",
+        }
+    ]      
+
     return(
         <div>
-            <h1>Carro de ID: 0</h1>
-            <button onClick={() => router.push('/carro/1')}>
-                Change ID
-            </button>
+            <h1>Carros</h1>
+            <DataTable data={data} columns={columns}></DataTable>
         </div>
     )
 }
