40eaf65cba2d189c4b62bdfa7b132a1506e89842
karengoncalves8 <karengoncalves128@gmail.com>
2025-03-30 18:40:17 -0300

    refactor: excluido pasta inutilizada

--- ESTADO_ATUAL/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// pages/_app.tsx
-import '../styles/global.css';
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
