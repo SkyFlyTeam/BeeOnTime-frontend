002d5d6d42de05b59c8ae2b253b98d80657f0a6f
Eric Lourenco <ericloumendes@gmail.com>
2025-04-01 23:49:17 -0300

    hotfix: Ajustando edição usuário primordial

---
+++ src/components/custom/CardEditarFuncionario/editarFuncionarioForm.tsx
@@ -0,0 +1,219 @@
+"use client";
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+// import { Switch } from "@/components/ui/switch"; // Assuming a switch component for 'Status'
+import { Button } from "@/components/ui/button";
+import { usuarioServices } from "@/services/usuarioServices";
+import { useEffect, useState } from "react";
+import { Usuario } from "@/interfaces/usuario";
+
+const formSchema = z.object({
+  nome: z.string().min(1, { message: "Campo obrigatório." }),
+  cpf: z.string().min(1, { message: "Campo obrigatório." }),
+  email: z.string()
+    .min(1, { message: "Campo obrigatório." })
+    .email({ message: "Por favor, insira um e-mail válido." }),
+  dataNascimento: z.string().min(1, { message: "Campo obrigatório." }),
+  dataContratacao: z.string().min(1, { message: "Campo obrigatório." }),
+  tipoContrato: z.string().min(1, { message: "Campo obrigatório." }),
+  cargo: z.string().min(1, { message: "Campo obrigatório." }),
+  nivelAcesso: z.string().min(1, { message: "Campo obrigatório." }),
+  setor: z.string().min(1, { message: "Campo obrigatório." }),
+  status: z.boolean(),
+});
+
+interface EditarFuncionarioFormProps {
+    usuarioInfo: Usuario;
+  }
+
+export default function EditarFuncionarioForm({usuarioInfo}: EditarFuncionarioFormProps) {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nome: usuarioInfo.usuario_nome,
+      cpf: usuarioInfo.usuario_cpf,
+      email: usuarioInfo.usuario_email,
+      dataNascimento: usuarioInfo.usuario_DataNascimento,
+      dataContratacao: usuarioInfo.usuario_dataContratacao,
+      tipoContrato: "",
+      cargo: usuarioInfo.usuario_cargo,
+      nivelAcesso: "",
+      setor: "",
+      status: true,
+    },
+  });
+
+  return (
+    <div className="border p-6" style={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "center" }}>
+      <Form {...form}>
+        <form>
+          <div className="flex flex-row flex-wrap gap-6">
+            {/* Status Toggle */}
+            <FormField
+              control={form.control}
+              name="status"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Status</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center">
+                      {/* <Switch checked={field.value} onCheckedChange={field.onChange} /> */}
+                      <span className="ml-3">{field.value ? "Usuário ativo" : "Usuário inativo"}</span>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Name */}
+            <FormField
+              control={form.control}
+              name="nome"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* CPF */}
+            <FormField
+              control={form.control}
+              name="cpf"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>CPF</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Email */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Data de Nascimento */}
+            <FormField
+              control={form.control}
+              name="dataNascimento"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Data de Nascimento</FormLabel>
+                  <FormControl>
+                    <Input type="date" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Data de Contratação */}
+            <FormField
+              control={form.control}
+              name="dataContratacao"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Data de Contratação</FormLabel>
+                  <FormControl>
+                    <Input type="date" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Tipo de Contrato */}
+            <FormField
+              control={form.control}
+              name="tipoContrato"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tipo de Contrato</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Cargo */}
+            <FormField
+              control={form.control}
+              name="cargo"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Cargo</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Nível de Acesso */}
+            <FormField
+              control={form.control}
+              name="nivelAcesso"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nível de Acesso</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Setor */}
+            <FormField
+              control={form.control}
+              name="setor"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Setor</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="flex justify-end mt-6">
+            <Button type="submit" className="bg-yellow-500 hover:bg-yellow-400 text-white">
+              Salvar
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
--- src/pages/historico-ponto/[id].tsx
+++ src/pages/historico-ponto/[id].tsx
@@ -11,19 +11,20 @@ import { usuarioServices } from "@/services/usuarioServices";
 import { Usuario } from "@/interfaces/usuario";
 import { getUsuario } from "@/services/authService";
 
-import { useRouter } from 'next/router';
-
+import { useRouter } from "next/router";
+import EditarFuncionarioForm from "@/components/custom/CardEditarFuncionario/editarFuncionarioForm";
 
 export default function PointsHistoryPage() {
-  //Simulando o diferente acesso
-  const[accessLevel, setAccessLevel] = useState<"USER" | "ADM">("USER")
-  const[ histPontos, setHistPontos ] = useState<RelatorioPonto[] | null>(null)
-  const[ usuarioInfo, setUsuarioInfo ] = useState<Usuario | null>(null)
+  // Simulando o diferente acesso
+  const [accessLevel, setAccessLevel] = useState<"USER" | "ADM">("USER");
+  const [histPontos, setHistPontos] = useState<RelatorioPonto[] | null>(null);
+  const [usuarioInfo, setUsuarioInfo] = useState<Usuario | null>(null);
+
+  // Loading state
+  const [isLoading, setIsLoading] = useState(true);
 
   const router = useRouter();
   const { id } = router.query;
-
-  // const { usuarioCargo, usuarioCod } = useAuth(); 
 
   const handleEdit = (entry: RelatorioPonto) => {
     // Lógica para editar a entrada (ex.: abrir um modal)
@@ -33,14 +34,10 @@ export default function PointsHistoryPage() {
   // Função para combinar as horas e os pontos
   const fetchHistPontos = async (usuario_cod: number) => {
     try {
-      // Buscar os pontos do usuário
       const pontos = await pontoServices.getPontosByUsuario(usuario_cod);
-      // Buscar as horas do usuário
       const horas = await horasServices.getHorasByUsuario(usuario_cod);
 
-      // Combinar as informações pelo 'horasCod'
       const combinedData = pontos.map((ponto: any) => {
-        // Encontrar o item correspondente de horas baseado no 'horasCod'
         const hora = horas.find((hora: any) => hora.horasCod === ponto.horasCod);
 
         return {
@@ -49,12 +46,11 @@ export default function PointsHistoryPage() {
           horasTrabalhadas: hora?.horasTrabalhadas || 0,
           horasNoturnas: hora?.horasNoturnas || 0,
           horasFaltantes: hora?.horasFaltantes || 0,
-          horasData: hora?.horasData || '',
+          horasData: hora?.horasData || "",
           usuarioCod: hora?.usuarioCod || 0,
         };
       });
 
-      // Atualizar o estado com os dados combinados
       setHistPontos(combinedData);
     } catch (error) {
       console.log("Erro ao recuperar histórico de pontos do usuário de id " + usuario_cod);
@@ -64,49 +60,50 @@ export default function PointsHistoryPage() {
   const fetchUsuario = async (usuario_cod: number) => {
     try {
       const usuario_data = await usuarioServices.getUsuarioById(usuario_cod);
-      setUsuarioInfo(usuario_data)
+      setUsuarioInfo(usuario_data);
     } catch (error) {
       console.log("Erro ao recuperar usuário de id " + usuario_cod);
     }
   };
-  
-    const getUser = async() => {
-      const user = await getUsuario();
-      console.log (user);
-      return user.data;
-    }
 
-  // Utilize useEffect para chamar a função quando o componente for montado
+  const getUser = async () => {
+    const user = await getUsuario();
+    return user.data;
+  };
+
   useEffect(() => {
-
-    const onMount = async() => {
+    const onMount = async () => {
       const usuario = await getUser();
 
       if (usuario.nivelAcesso.nivelAcesso_cod == 2) {
-      fetchHistPontos(usuario.usuario_cod);
-      fetchUsuario(usuario.usuario_cod);
-      }
-      else {
-        setAccessLevel('ADM')
+        fetchHistPontos(usuario.usuario_cod);
+        fetchUsuario(usuario.usuario_cod);
+      } else {
+        setAccessLevel("ADM");
         fetchHistPontos(parseInt(id!.toString()));
         fetchUsuario(parseInt(id!.toString()));
       }
-    }
+      setIsLoading(false); // Set loading to false after data is fetched
+    };
 
-    onMount()
+    onMount();
+  }, []); // Empty dependency array ensures the effect runs once after mount
 
-  }, []);
+  if (isLoading) {
+    return <div>Carregando...</div>; // Show loading state while data is being fetched
+  }
 
   return (
-    <div className="flex flex-col  p-6 md:p-9">
+    <div className="flex flex-col p-6 md:p-9">
+      {/* {accessLevel == "ADM" ? <EditarFuncionarioForm usuarioInfo={usuarioInfo!} /> : null} */}
       <h1 className="text-xl md:text-3xl font-semibold mb-4">
         {accessLevel === "USER" ? "Meus Pontos" : `Pontos de ${usuarioInfo?.usuario_nome}`}
       </h1>
-      <PointsHistoryTable 
-        entries={histPontos} 
+      <PointsHistoryTable
+        entries={histPontos}
         userInfo={usuarioInfo}
         onEdit={handleEdit}
-        accessLevel={accessLevel} 
+        accessLevel={accessLevel}
       />
     </div>
   );
