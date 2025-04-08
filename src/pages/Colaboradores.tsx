4abc8e2de1f32b91b733ee3eec9ab0bfd9b78c79
andresalerno <andresalerno22@gmail.com>
2025-04-08 18:09:30 -0300

    hotfix: adicionando a barra de navegação

--- src/pages/Colaboradores.tsx
+++ src/pages/Colaboradores.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 import { Table, TableHeader, TableBody, TableFooter, TableHead, TableRow, TableCell, TableCaption } from "@/components/listagem";
 import { toast, ToastContainer } from "react-toastify";
@@ -14,12 +14,14 @@ import { useRouter } from 'next/router';
 import { getUsuario } from "@/services/authService";
 
 export default function Colaboradores() {
-  const [usuarios, setUsuarios] = useState<UsuarioInfo[]>([]); // Garante que seja um array vazio inicialmente
+  const [usuarios, setUsuarios] = useState<UsuarioInfo[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
   const [thisUser, setThisUser] = useState<UsuarioInfo>();
-
+  
   const [isOpen, setIsOpen] = useState(false); 
+  const [currentPage, setCurrentPage] = useState(1); // Página atual
+  const itemsPerPage = 5; // Itens por página
 
   const router = useRouter();
 
@@ -35,18 +37,18 @@ export default function Colaboradores() {
     }
   };
 
-      const getUser = async() => {
-        const user = await getUsuario();
-        console.log (user);
-        return user.data;
-      }
+  const getUser = async() => {
+    const user = await getUsuario();
+    console.log (user);
+    return user.data;
+  }
 
   useEffect(() => {
     fetchUsuarios();
   }, []);
 
   const showToast = (success: boolean) => {
-      success ? showSucessToast() : showErrorToast();
+    success ? showSucessToast() : showErrorToast();
   }
 
   const showSucessToast = () => {
@@ -71,6 +73,35 @@ export default function Colaboradores() {
     console.log(`Ver detalhes do usuário com ID: ${usuarioId}`);
     router.push(`/historico-ponto/${usuarioId}`)
   };
+
+  // Função para mudar a página
+  const handlePageChange = (page: number) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  // Função para navegar para a página anterior
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  // Função para navegar para a página seguinte
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Calcular os itens da página atual
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = usuarios.slice(startIndex, endIndex);
+
+  // Total de páginas
+  const totalPages = Math.ceil(usuarios.length / itemsPerPage);
 
   if (loading) {
     return <div>Carregando...</div>;
@@ -114,72 +145,66 @@ export default function Colaboradores() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {usuarios.length > 0 ? (
-                  usuarios.map((usuario, index) => ( (usuario.usuario_cod !== thisUser?.usuario_cod && usuario.nivelAcesso.nivelAcesso_cod !== 0) ? (
-                    <TableRow
-                      key={index}
-                      className={index % 2 === 0 ? "bg-[#FFF8E1]" : "bg-[#FFFFFF]"}
-                    >
-                      <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.usuario_nome}</TableCell>
-                      <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.usuario_cargo}</TableCell>
-                      <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.setor?.setorNome}</TableCell>
-                      <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.usuario_cargaHoraria}</TableCell>
-                      <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.usuarioTipoContratacao}</TableCell>
-                      <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.nivelAcesso?.nivelAcesso_nome}</TableCell>
-                      <TableCell className="border-r border-b border-gray-200 text-center justify-center flex">
-                        <button 
-                          onClick={() => handleViewUser(usuario.usuario_cod)} 
-                          className="bg-[#FFB503] rounded-md p-2 hover:bg-orange-600">
-                          <FontAwesomeIcon icon={faEye} className="text-black-600"/>
-                        </button>
-                      </TableCell>
-                    
-                    </TableRow>
-                  ): null))
+                {currentItems.length > 0 ? (
+                  currentItems.map((usuario, index) => (
+                    (usuario.usuario_cod !== thisUser?.usuario_cod && usuario.nivelAcesso.nivelAcesso_cod !== 0) ? (
+                      <TableRow
+                        key={index}
+                        className={index % 2 === 0 ? "bg-[#FFF8E1]" : "bg-[#FFFFFF]"}
+                      >
+                        <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.usuario_nome}</TableCell>
+                        <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.usuario_cargo}</TableCell>
+                        <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.setor?.setorNome}</TableCell>
+                        <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.usuario_cargaHoraria}</TableCell>
+                        <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.usuarioTipoContratacao}</TableCell>
+                        <TableCell className="border border-gray-200 text-center text-black text-base p-3">{usuario.nivelAcesso?.nivelAcesso_nome}</TableCell>
+                        <TableCell className="border-r border-b border-gray-200 text-center justify-center flex">
+                          <button 
+                            onClick={() => handleViewUser(usuario.usuario_cod)} 
+                            className="bg-[#FFB503] rounded-md p-2 hover:bg-orange-600">
+                            <FontAwesomeIcon icon={faEye} className="text-black-600"/>
+                          </button>
+                        </TableCell>
+                      </TableRow>
+                    ) : null
+                  ))
                 ) : (
                   <TableRow>
-                    <TableCell colSpan={6} className="text-center">Nenhum colaborador encontrado</TableCell>
+                    <TableCell colSpan={7} className="text-center">Nenhum colaborador encontrado</TableCell>
                   </TableRow>
                 )}
               </TableBody>
             </Table>
           </div>
 
-          {/* Mobile - Tabela deitada com mesmo estilo */}
-          <div className="block md:hidden overflow-x-auto">
-            <table className="min-w-[900px] w-full border-collapse text-sm text-black">
-              <tbody>
-                {[
-                  { label: "NOME", render: (usuario: UsuarioInfo) => usuario.usuario_nome },
-                  { label: "CARGO", render: (usuario: UsuarioInfo) => usuario.usuario_cargo },
-                  { label: "SETOR", render: (usuario: UsuarioInfo) => usuario.setor?.setorNome },
-                  { label: "CARGA HORÁRIA DIÁRIA", render: (usuario: UsuarioInfo) => usuario.usuario_cargaHoraria },
-                  { label: "CONTRATO", render: (usuario: UsuarioInfo) => usuario.usuarioTipoContratacao },
-                  { label: "NÍVEL ACESSO", render: (usuario: UsuarioInfo) => usuario.nivelAcesso?.nivelAcesso_nome },
-                  { label: "AÇÕES", render: (usuario: UsuarioInfo) => ( <button onClick={() => handleViewUser(usuario.usuario_cod)} 
-                  className="bg-[#FFB503] rounded-md p-2 hover:bg-orange-600">
-                  <FontAwesomeIcon icon={faEye} className="text-black-600"/>
-                </button>) }
-                ].map((row, idx) => (
-                  <tr key={idx}>
-                    <td className="border border-gray-200 p-3 font-semibold">{row.label}</td>
-                    {usuarios.length > 0 ? (
-                      usuarios.map((usuario, i) => ( (usuario.usuario_cod !== thisUser?.usuario_cod && usuario.nivelAcesso.nivelAcesso_cod !== 0) ? (
-                        <td
-                          key={i}
-                          className={`border border-gray-200 p-3 text-center text-base ${i % 2 === 0 ? "bg-[#FFF8E1]" : "bg-[#FFFFFF]"}
-                          `}
-                        >
-                          {row.render(usuario)}
-                        </td>
-                      ): null))
-                    ) : (
-                      <td colSpan={6} className="text-center">Nenhum colaborador encontrado</td>
-                    )}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          {/* Paginação */}
+          <div className="flex justify-end mt-4">
+            <button 
+              className="px-4 py-2 rounded-md text-gray mr-2"
+              onClick={handlePreviousPage}
+              disabled={currentPage === 1}
+            >
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+
+            {/* Números das páginas */}
+            {[...Array(totalPages)].map((_, idx) => (
+              <button 
+                key={idx + 1}
+                className={`px-4 py-2 rounded-md ${currentPage === idx + 1 ? "text-gray" : "bg-white text-black"} hover:bg-[#FFB503]`}
+                onClick={() => handlePageChange(idx + 1)}
+              >
+                {idx + 1}
+              </button>
+            ))}
+
+            <button 
+              className="px-4 py-2 rounded-md text-gray ml-2"
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages}
+            >
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
           </div>
         </div>
       </div>
@@ -190,6 +215,5 @@ export default function Colaboradores() {
 
       <ToastContainer position="top-center" autoClose={3000} />
     </div>
-
   );
 }
