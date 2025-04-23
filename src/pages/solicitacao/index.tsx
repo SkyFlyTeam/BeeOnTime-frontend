4d26bd7ee2e02e9940e0cbc737031b266852c3c8
brennolyrio <brennolyrio@gmail.com>
2025-04-23 18:36:00 -0300

    feat: Botao Dropdown Solicitacao Criado

--- src/pages/solicitacao/index.tsx
+++ src/pages/solicitacao/index.tsx
@@ -1,147 +1,152 @@
-import { useEffect, useState } from 'react'
-import { ApiException } from '../../config/apiExceptions'
-import { solicitacaoServices } from '../../services/solicitacaoServices'
-import SolicitationCard from './SolicitacaoCard'
-import styles from './Solicitacao.module.css'
-import Tab from '../../components/custom/tab'
-import Modal from '../../components/custom/modalSolicitacao/index'
-import SolicitacaoInterface from '../../interfaces/Solicitacao'
-import ModalDevolutiva from '../../components/custom/modalSolicitacao/modalDevolutiva'
-import { getUsuario } from '../../services/authService'
-import ModalAjustePonto from '@/components/custom/modalSolicitacao/modalAjustePonto'
-import ModalDecisaoHoraExtra from '@/components/custom/modalSolicitacao/modalHoraExtra/modalHoraExtra'
-import { renderModalChildren } from '../../utils/renderModalByTipoSolicitacao.tsx'
-
+import { useEffect, useState } from 'react';
+import { ApiException } from '../../config/apiExceptions';
+import { solicitacaoServices } from '../../services/solicitacaoServices';
+import SolicitationCard from './SolicitacaoCard';
+import styles from './Solicitacao.module.css';
+import Tab from '../../components/custom/tab';
+import Modal from '../../components/custom/modalSolicitacao/index';
+import SolicitacaoInterface from '../../interfaces/Solicitacao';
+import ModalDevolutiva from '../../components/custom/modalSolicitacao/modalDevolutiva';
+import { getUsuario } from '../../services/authService';
+import { renderModalChildren } from '../../utils/renderModalByTipoSolicitacao.tsx';
+import BotaoDropdownSolicitacao from '../../components/custom/BotaoSolicitacao/dropdownSolicitacao'; // Ajuste o caminho conforme necessário
 
 interface SolicitacoesState {
-  all: SolicitacaoInterface[]
-  pendentes: SolicitacaoInterface[]
-  historico: SolicitacaoInterface[]
+  all: SolicitacaoInterface[];
+  pendentes: SolicitacaoInterface[];
+  historico: SolicitacaoInterface[];
 }
 
 const Solicitacao = () => {
   // Modais
-  const [openDevolutivaModal, setOpenDevolutivaModal] = useState<boolean>(false)
-  const [isModalHoraExtraOpen, setIsModalHoraExtraOpen] = useState(false);
-  const [openModal, setOpenModal] = useState<{
-    [key: string]: boolean
-  }>({})
+  const [openDevolutivaModal, setOpenDevolutivaModal] = useState<boolean>(false);
+  const [openModal, setOpenModal] = useState<{ [key: string]: boolean }>({});
+  
 
   // Informações do usuário
-  const [usuarioCod, setUsuarioCod] = useState<number>(0)
-  const [usuarioCargo, setUsuarioCargo] = useState<string>('')
-  const [nivelAcessoCod, setNivelAcessoCod] = useState<number>()
-  const [setorCod, setSetorCod] = useState()
+  const [usuarioCod, setUsuarioCod] = useState<number>(0);
+  const [usuarioCargo, setUsuarioCargo] = useState<string>('');
+  const [nivelAcessoCod, setNivelAcessoCod] = useState<number>();
+  const [setorCod, setSetorCod] = useState();
 
-  const [toogle, setToogle] = useState(false)
+  const [toogle, setToogle] = useState(false);
 
   const [solicitacoesData, setSolicitacoesData] = useState<SolicitacoesState>({
     all: [],
     pendentes: [],
     historico: [],
-  })
+  });
 
   // Paginação
-  const [displayedSolicitacoes, setDisplayedSolicitacoes] = useState<SolicitacaoInterface[]>([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [itemsPerPage, setItemsPerPage] = useState(5)
-  const [totalItems, setTotalItems] = useState(0)
+  const [displayedSolicitacoes, setDisplayedSolicitacoes] = useState<SolicitacaoInterface[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [totalItems, setTotalItems] = useState(0);
 
   const paginateData = () => {
-    const dataToDisplay = toogle ? solicitacoesData.pendentes : solicitacoesData.historico
-    const startIndex = (currentPage - 1) * itemsPerPage
-    const endIndex = startIndex + itemsPerPage
-    setDisplayedSolicitacoes(dataToDisplay.slice(startIndex, endIndex))
-  }  
+    const dataToDisplay = toogle ? solicitacoesData.pendentes : solicitacoesData.historico;
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    setDisplayedSolicitacoes(dataToDisplay.slice(startIndex, endIndex));
+  };
 
   const handlePageChange = (page: number) => {
-    setCurrentPage(page)
-  }
+    setCurrentPage(page);
+  };
 
   const handleModal = (solicitacaoCod: number, status: boolean) => {
     setOpenModal((prevState) => ({
       ...prevState,
       [solicitacaoCod]: status,
-    }))
+    }));
 
     if (status === true) {
-      handleDevolutivaModal(false)
+      handleDevolutivaModal(false);
     }
-  }
+  };
 
   const handleDevolutivaModal = (status: boolean) => {
-    setOpenDevolutivaModal(status)
-  }
+    setOpenDevolutivaModal(status);
+  };
 
   const handleClick = (status: 'pendentes' | 'historico') => {
-    setCurrentPage(1)
+    setCurrentPage(1);
 
     setSolicitacoesData((prevState) => ({
       ...prevState,
       all: prevState[status],
-    }))
-    setToogle(status === 'pendentes')
-  }
+    }));
+    setToogle(status === 'pendentes');
+  };
 
   // Atualizar lista
   const handleSolicitacaoUpdate = async (updatedSolicitacao: SolicitacaoInterface) => {
     setSolicitacoesData((prevData) => {
       const updatedPendentes = prevData.pendentes.filter(
         (solicitacao) => solicitacao.solicitacaoCod !== updatedSolicitacao.solicitacaoCod
-      )
+      );
       const updatedHistorico = prevData.historico.filter(
         (solicitacao) => solicitacao.solicitacaoCod !== updatedSolicitacao.solicitacaoCod
-      )
-  
+      );
+
       if (updatedSolicitacao.solicitacaoStatus === 'PENDENTE') {
-        updatedPendentes.push(updatedSolicitacao)
+        updatedPendentes.push(updatedSolicitacao);
       } else {
-        updatedHistorico.push(updatedSolicitacao)
+        updatedHistorico.push(updatedSolicitacao);
       }
-  
+
       return {
         ...prevData,
         pendentes: updatedPendentes,
         historico: updatedHistorico,
-      }
-    })
-    
-    paginateData()
-  }
-  
-  // Atualziar depois de uma exclusão
+      };
+    });
+
+    paginateData();
+  };
+
+  // Atualizar depois de uma exclusão
   const handleDeleteSolicitacao = (idToDelete: number) => {
     setSolicitacoesData((prevData) => {
       const updatedPendentes = prevData.pendentes.filter(
         (solicitacao) => solicitacao.solicitacaoCod !== idToDelete
-      )
+      );
       const updatedHistorico = prevData.historico.filter(
         (solicitacao) => solicitacao.solicitacaoCod !== idToDelete
-      )
+      );
       const updatedAll = prevData.all.filter(
         (solicitacao) => solicitacao.solicitacaoCod !== idToDelete
-      )
+      );
       return {
         ...prevData,
         pendentes: updatedPendentes,
         historico: updatedHistorico,
         all: updatedAll,
-      }
-    })
+      };
+    });
 
     setDisplayedSolicitacoes((prev) =>
       prev.filter((solicitacao) => solicitacao.solicitacaoCod !== idToDelete)
-    )
-  }
+    );
+  };
 
-  const fetchSolicitacoes = async (usuarioCargo: string, usuarioCod: number, nivelAcessoCod: number, setorCod: number) => {
-    let result: SolicitacaoInterface[] | ApiException | null = null
+  const fetchSolicitacoes = async (
+    usuarioCargo: string,
+    usuarioCod: number,
+    nivelAcessoCod: number,
+    setorCod: number
+  ) => {
+    let result: SolicitacaoInterface[] | ApiException | null = null;
     if (nivelAcessoCod === 2) {
-      result = await solicitacaoServices.getAllSolicitacaoByUsuario(usuarioCod) as SolicitacaoInterface[] | ApiException
+      result = (await solicitacaoServices.getAllSolicitacaoByUsuario(
+        usuarioCod
+      )) as SolicitacaoInterface[] | ApiException;
     } else if (nivelAcessoCod === 1) {
-      result = await solicitacaoServices.getAllSolicitacaoBySetor(setorCod) as SolicitacaoInterface[] | ApiException
+      result = (await solicitacaoServices.getAllSolicitacaoBySetor(
+        setorCod
+      )) as SolicitacaoInterface[] | ApiException;
     } else {
-      result = await solicitacaoServices.getAllSolicitacao()
+      result = await solicitacaoServices.getAllSolicitacao();
     }
 
     if (result instanceof ApiException || result === null) {
@@ -149,57 +154,63 @@ const Solicitacao = () => {
         all: [],
         pendentes: [],
         historico: [],
-      })
-      setTotalItems(0)
+      });
+      setTotalItems(0);
     } else {
-  
       setSolicitacoesData({
         all: result.filter((s: SolicitacaoInterface) => s.solicitacaoStatus !== 'PENDENTE'),
         pendentes: result.filter((s: SolicitacaoInterface) => s.solicitacaoStatus === 'PENDENTE'),
         historico: result.filter((s: SolicitacaoInterface) => s.solicitacaoStatus !== 'PENDENTE'),
-      })
-  
-      setTotalItems(result.length)
+      });
+
+      setTotalItems(result.length);
     }
-  }
+  };
 
   useEffect(() => {
     const initialize = async () => {
       try {
-        const response = await getUsuario()
+        const response = await getUsuario();
         if (!response || !response.data) {
-          console.error('Usuário não encontrado.')
-          return
+          console.error('Usuário não encontrado.');
+          return;
         }
-  
-        const { usuario_cod, usuario_cargo, nivelAcesso_cod, setorCod } = response.data
-        setUsuarioCod(usuario_cod)
-        setUsuarioCargo(usuario_cargo)
-        setNivelAcessoCod(nivelAcesso_cod)
-        setSetorCod(setorCod)
 
-        await fetchSolicitacoes(usuario_cargo, usuario_cod, nivelAcesso_cod, setorCod)
+        const { usuario_cod, usuario_cargo, nivelAcesso_cod, setorCod } = response.data;
+        setUsuarioCod(usuario_cod);
+        setUsuarioCargo(usuario_cargo);
+        setNivelAcessoCod(nivelAcesso_cod);
+        setSetorCod(setorCod);
+
+        await fetchSolicitacoes(usuario_cargo, usuario_cod, nivelAcesso_cod, setorCod);
       } catch (error) {
-        console.error('Erro ao obter usuário:', error)
+        console.error('Erro ao obter usuário:', error);
       }
-    }
-  
-    initialize()
-    }, [usuarioCod, usuarioCargo, nivelAcessoCod, setorCod])  
-    
-    useEffect(() => {
-      paginateData()  
-    }, [currentPage, solicitacoesData, toogle])
-  
+    };
+
+    initialize();
+  }, [usuarioCod, usuarioCargo, nivelAcessoCod, setorCod]);
+
+  useEffect(() => {
+    paginateData();
+  }, [currentPage, solicitacoesData, toogle]);
 
   const totalPages = Math.ceil(
     (toogle ? solicitacoesData.pendentes.length : solicitacoesData.historico.length) / itemsPerPage
-  )
+  );
 
   return (
     <div className={styles.solicitacao_container}>
       <div className={styles.card_container}>
-        <h1 className='font-bold text-4xl'>Solicitações</h1>
+        <h1 className="font-bold text-4xl">Solicitações</h1>
+
+        {/* Componente do dropdown e modais */}
+        <BotaoDropdownSolicitacao
+          usuarioCod={usuarioCod}
+          usuarioCargo={usuarioCargo}
+          handleSolicitacaoUpdate={handleSolicitacaoUpdate}
+        />
+
         <Tab
           toogle={toogle}
           onClick={handleClick}
@@ -218,7 +229,7 @@ const Solicitacao = () => {
                   usuarioCod={solicitacao.usuarioCod}
                   usuarioLogadoCargo={usuarioCargo}
                   usuarioLogadoCod={usuarioCod}
-                  onDelete={handleDeleteSolicitacao} 
+                  onDelete={handleDeleteSolicitacao}
                 />
 
                 <Modal
@@ -227,17 +238,16 @@ const Solicitacao = () => {
                   solicitacao={solicitacao}
                   onSolicitacaoUpdate={handleSolicitacaoUpdate}
                   usuarioLogadoCod={usuarioCod}
-                  usuarioCargo={usuarioCargo} 
+                  usuarioCargo={usuarioCargo}
                   children={renderModalChildren({
                     solicitacao,
                     onSolicitacaoUpdate: handleSolicitacaoUpdate,
                     onClose: () => handleModal(solicitacao.solicitacaoCod, false),
                     usuarioLogadoCod: usuarioCod,
                     usuarioCargo: usuarioCargo,
-                    nivelAcessoCod: nivelAcessoCod
-                  })
-                  } 
-                  title={solicitacao.tipoSolicitacaoCod.tipoSolicitacaoNome}                  
+                    nivelAcessoCod: nivelAcessoCod,
+                  })}
+                  title={solicitacao.tipoSolicitacaoCod.tipoSolicitacaoNome}
                 />
               </div>
             ))
@@ -255,11 +265,11 @@ const Solicitacao = () => {
                 onClick={() => handlePageChange(currentPage - 1)}
                 disabled={currentPage === 1}
               >
-                &lt;
+                {'<'}
               </button>
 
               {Array.from({ length: totalPages }, (_, index) => {
-                const page = index + 1
+                const page = index + 1;
                 if (page === 1 || page === totalPages || Math.abs(page - currentPage) <= 1) {
                   return (
                     <button
@@ -269,14 +279,14 @@ const Solicitacao = () => {
                     >
                       {page}
                     </button>
-                  )
+                  );
                 } else if (Math.abs(page - currentPage) === 2 && page < currentPage) {
-                  return <span key={`left-ellipsis`} className={styles.ellipsis}>...</span>
+                  return <span key={`left-ellipsis`} className={styles.ellipsis}>...</span>;
                 } else if (Math.abs(page - currentPage) === 2 && page > currentPage) {
-                  return <span key={`right-ellipsis`} className={styles.ellipsis}>...</span>
+                  return <span key={`right-ellipsis`} className={styles.ellipsis}>...</span>;
                 }
 
-                return null
+                return null;
               })}
 
               <button
@@ -284,14 +294,14 @@ const Solicitacao = () => {
                 onClick={() => handlePageChange(currentPage + 1)}
                 disabled={currentPage === totalPages}
               >
-                &gt;
+                {'>'}
               </button>
             </div>
           )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Solicitacao
+export default Solicitacao;
---
+++ src/pages/solicitacao/testeModal.tsx
@@ -0,0 +1,24 @@
+export function IsModalFeriasOpen({
+    isOpen,
+    onClose,
+  }: {
+    isOpen: boolean;
+    onClose: () => void;
+  }) {
+    if (!isOpen) return null;
+  
+    return (
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+        <div className="bg-white p-6 rounded-md">
+          <h2 className="text-xl font-bold mb-4">Solicitar Férias</h2>
+          <p>Este é um modal de teste para Férias!</p>
+          <button
+            className="mt-4 bg-red-500 text-white px-4 py-2 rounded-md"
+            onClick={onClose}
+          >
+            Fechar
+          </button>
+        </div>
+      </div>
+    );
+  }
