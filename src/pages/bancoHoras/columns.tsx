b0f1a2d82f5e227acd0944710cd5bc13a9bced12
Karen Gonçalves <105525498+karengoncalves8@users.noreply.github.com>
2025-05-11 18:34:22 -0300

    fix: conversão de valores de horas float para int e inclusão do perfil na sidebar (#120)

--- src/pages/bancoHoras/columns.tsx
+++ src/pages/bancoHoras/columns.tsx
@@ -12,7 +12,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 const formatStringToHour = (value: string) => {
-  return `${value}h`
+  let intValue = parseInt(value, 10);
+  return `${intValue}h`
 }
 
 export const columnsAdmin: ColumnDef<bancoHorasMensalAdmin>[] = [
