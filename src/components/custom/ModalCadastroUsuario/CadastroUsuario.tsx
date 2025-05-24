fa708070997d84ff582001ef86af235a088c82f2
Gustavo Muraoka Silva <gustavomuraoka15@gmail.com>
2025-05-24 12:04:56 -0300

    hotfix: alterando empCod default

--- src/components/custom/ModalCadastroUsuario/CadastroUsuario.tsx
+++ src/components/custom/ModalCadastroUsuario/CadastroUsuario.tsx
@@ -57,7 +57,7 @@ export default function CadastroUsuario({ onClose, onSave }: { onClose: () => vo
     usuario_DataNascimento: "",
     usuario_cargo: "",
     setorCod: "",
-    empCod: 2,
+    empCod: 1,
     nivelAcesso_cod: ""
   });
   const [formErrors, setFormErrors] = useState<any>({});
